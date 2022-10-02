17a706d556307b3c41b401a9dfb563f9ca1df9ed
unknown <luisangelmaita@gmail.com>
2022-10-01 23:24:18 -0300

    oro

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -3,7 +3,7 @@
 const highlightjs = require;
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = hay;
+const nuevaString = oro;
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = null;
