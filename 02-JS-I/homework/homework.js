3e033e3a47eb6cec44cc29b8cdf2f5ad01fcb073
Luis Angel Maita <luisangelmaita@gmail.com>
2022-10-09 23:43:15 -0300

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -3,7 +3,8 @@
 const highlightjs = require;
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = hola;
+const nuevaString = "Angel";
+
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = null;
